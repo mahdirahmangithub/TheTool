c70ab13e74d94bbb9bd51438d5a2efb844a21274
Mostafa Shamsitabar <mosio@Mostafas-iMac.local>
2019-07-27 11:41:40 +0430

    accordion | fully implemented

--- Components/src/assets/js/collapsable.js
+++ Components/src/assets/js/collapsable.js
@@ -6,14 +6,20 @@ jQuery(function($) {
     var $target = $($(this).attr("data-target"));
     var _animation = $(this).attr("data-animation");
 
+    $(this).toggleClass("active");
+
     if ($target.hasClass("show")) {
-      _animation === "slide-lr" ? $target.hide(360) : $target.slideUp(360);
+      _animation === "slide-horizontal"
+        ? $target.hide(360)
+        : $target.slideUp(240);
       $target.removeClass("show collapse").addClass("collapsing");
       $target.removeClass("collapsing").addClass("collapse");
     } else {
-      _animation === "slide-lr" ? $target.show(360) : $target.slideDown(360);
-      $target.removeClass("collapsing").addClass("collapse show");
+      _animation === "slide-horizontal"
+        ? $target.show(360)
+        : $target.slideDown(240);
       $target.removeClass("collapse").addClass("collapsing");
+      $target.removeClass("collapsing").addClass("collapse show");
     }
   });
 });
